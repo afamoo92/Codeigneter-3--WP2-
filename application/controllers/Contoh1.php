7cc4655cf3fcfbfdb62f5bc9429d762c6c111f48
afamoo92 <alfajri.mulya0903@gmail.com>
2021-09-25 01:45:57 +0700

    commit pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Al Fajri Mulya<br>
+             Saya tinggal di daerah Jatibening<br>
+             Hobi saya bermain Game Online";
+    }
+}           
